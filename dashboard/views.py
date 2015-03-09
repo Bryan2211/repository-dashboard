da4e2f23483de29d3ffead024c87f984d2c2c0e2
bryan2211 <bryan.oberson@gmail.com>
2015-03-09 17:47:36 +0000

    Ajouté une view pour ajouter un élève et un prof dans une classe déjà existante et mis à jour la vue nouvelle classe

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.contrib.auth import authenticate, login, logout
-from dashboard.forms import NewGroupForm
+from dashboard.forms import NewGroupForm, NewStudentForm, NewTeacherForm
 from django.core.urlresolvers import reverse
 from common.models import Group, Teacher
 from django.contrib.auth.models import User
@@ -11,6 +11,17 @@ def home(request):
     return render_to_response('dashboard/templates/dashboard/index.html')
 
 def group(request):
+    if request.method == "POST":
+        formStudent = NewStudentForm(request.POST)
+        if formStudent.is_valid:
+            newStudent = formStudent.cleaned_data["newStudent"]
+            Group.student.add(newStudent)
+            Group.save()
+        formTeacher = NewTeacherForm(request.POST)
+        if formTeacher.is_valid:
+            newTeacher = formTeacher.cleaned_data["newTeacher"]
+            Group.teacher.add(newTeacher)
+            Group.save()
     return render_to_response('dashboard/templates/dashboard/class.html')
 
 def exercises(request):
@@ -21,8 +32,12 @@ def newgroup(request):
         form = NewGroupForm(request.POST)
         if form.is_valid():
             group_name = form.cleaned_data["group_name"]
-            teacher = Teacher
-            group = Group(group_name, teacher)
+            
+            group = Group(name = group_name)
+            group.save()
+            group.teacher = Teacher
             group.save()
             return HttpResponse("Classe corrrectement créée")
     return render(request, "dashboard/templates/dashboard/newclass.html", locals())
+    
+        
