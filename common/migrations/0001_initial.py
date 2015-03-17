10c59a7f45c0dbd29dc8f83744740a3204212d76
bryan2211 <bryan.oberson@gmail.com>
2015-03-17 20:16:52 +0000

    Réparé mes problèmes de modèle

--- common/migrations/0001_initial.py
+++ common/migrations/0001_initial.py
@@ -13,10 +13,32 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=30)),
+                ('created_on', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='GroupMembers',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('added_on', models.DateField(auto_now=True)),
+                ('group', models.ForeignKey(to='common.Group')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Student',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
-                ('avatar', models.ImageField(null=True, upload_to='avatars/', blank=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('avatar', models.ImageField(blank=True, upload_to='avatars/', null=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
@@ -27,13 +49,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Teacher',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
-                ('avatar', models.ImageField(null=True, upload_to='avatars/', blank=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('avatar', models.ImageField(blank=True, upload_to='avatars/', null=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'abstract': False,
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='groupmembers',
+            name='student',
+            field=models.ForeignKey(null=True, to='common.Student'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='groupmembers',
+            name='teacher',
+            field=models.ForeignKey(to='common.Teacher'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='student',
+            field=models.ManyToManyField(through='common.GroupMembers', to='common.Student'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='teacher',
+            field=models.ManyToManyField(through='common.GroupMembers', to='common.Teacher'),
+            preserve_default=True,
         ),
     ]
