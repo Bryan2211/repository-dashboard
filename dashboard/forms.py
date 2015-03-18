852a126872dda5059c5d292b9fcff8bde38b60b0
bryan2211 <bryan.oberson@gmail.com>
2015-03-18 21:50:34 +0000

    Remplacé tous les mots Classe par Groupe pour suivre la logique

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,7 +3,7 @@ from django.forms.extras.widgets import *
 
     
 class NewGroupForm(forms.Form):
-    group_name = forms.CharField(label='Nom de la classe', max_length = 30)
+    group_name = forms.CharField(label='Nom du groupe', max_length = 30)
 
 class NewStudentForm(forms.Form):
     nickname = forms.CharField(label="Nom de l'élève", max_length=30)
