89a8d5ed50163c3bd243c8a2ae2dee84ee9ce809
bryan2211 <bryan.oberson@gmail.com>
2015-03-23 18:27:38 +0000

    Retiré une url qui ne m'était plus utile

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,13 +6,10 @@ from dashboard.views import *
 urlpatterns = patterns('teachers.views',
     url(r'^home/$', home, name='home'),
     url(r'^classe/(?P<group_id>\d+)/$', group, name='group_view'),
-    url(r'^login/$', login, name='login'),
     url(r'^exercices/$', exercises, name='exercises'),
     url(r'^nouveau_groupe/$', newgroup, name='newgroup'),
     url(r'^profil/$', profil, name = 'profil'),
     url(r'^enlever_groupe/(?P<group_id>\d+)/(?P<member_id>\d+)/$', deleteFromGroup, name = "deleteFromGroup"),
     url(r'^enlever_activité/(?P<activity_id>\d+)/$', deleteActivity, name = "deleteActivity"),
     url(r'^enlever_devoir/(?P<group_id>\d+)/(?P<homework_id>\d+)/$', deleteHomework, name = "deleteHomework"),
-    #url(r'^register/$', register, name='register'),
-    #url(r'^admin/', include(admin.site.urls)),
 )
